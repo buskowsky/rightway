093ae7b0aaf574a0e48e1803a4b22c91366969da
Jakub Fonferko <buskowsky@protonmail.com>
2017-11-13 10:44:56 +0100

    initialize project

---
+++ src/main/java/com/jfonferko/rightway/RightwayApplication.java
@@ -0,0 +1,12 @@
+package com.jfonferko.rightway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RightwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RightwayApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jfonferko/rightway/RightwayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jfonferko.rightway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RightwayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
